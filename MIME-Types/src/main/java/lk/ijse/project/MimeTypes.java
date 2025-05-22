b62fd33abda5ecd98e118ce0946f5ed519cb3fea
DiilaNa <liyanaarachchidilan@gmail.com>
2025-05-22 11:43:08 +0530

    Mime-Types - multi part/form data

--- MIME-Types/src/main/java/lk/ijse/project/MimeTypes.java
+++ MIME-Types/src/main/java/lk/ijse/project/MimeTypes.java
@@ -1,16 +1,18 @@
 package lk.ijse.project;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
-
+@MultipartConfig
 @WebServlet("/customer")
 public class MimeTypes extends HttpServlet {
     //      read text / plain data from http request body
@@ -25,12 +27,25 @@ public class MimeTypes extends HttpServlet {
     }*/
 
     /*Read x-www-form-urlencoded data from a http req*/
-    @Override
+   /* @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
         String address = req.getParameter("address");
 
         resp.setContentType("text/plain");
         resp.getWriter().println(name + " " + address);
+    }*/
+
+
+    /*Read multipart/form data from a http request*/
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        Part part = req.getPart("file");
+        String filename = part.getSubmittedFileName();
+
+        resp.setContentType("text/plain");
+        resp.getWriter().write(name + " " + filename);
+
     }
 }
